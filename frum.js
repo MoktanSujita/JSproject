16dfeed1bfee3e6d0839715f06808852d5d3856d
Moktan Sujita <sujitamoktan77@gmail.com>
2024-09-21 01:06:08 +0545

    commit

--- frum.js
+++ frum.js
@@ -1,3 +1,4 @@
+
 const datafetch = async()=>             //arrow function
 {
     const d= document.getElementById("unlist"); // Moved this inside to ensure it runs after the DOM is loaded
@@ -5,11 +6,18 @@ const datafetch = async()=>             //arrow function
     const response =await fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => response.json());
     console.log("respose:",response);
-if (response) {
-    console.log("Data fetch successful");
-    const li = document.getElementById("li");
-    li.innerHTML = `<span>${response.title}</span> `;
-  }
-  d.appendChild(li);
+    if (response) 
+        {
+            console.log("Data fetch successful");
+             const li = document.createElement("li")
+             li.className ="li"; 
+             li.innerHTML = `<span>${response.title}</span> `;
+             d.appendChild(li);
+        }
 };
+ocument.getElementById("button").addEventListener("click", datafetch);
+
+            
+       
+
 
